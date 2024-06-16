030cd0ec6a1e278db80f602a4899f1b5131c74b5
BOSON-18 <deeshankbatra663@gmail.com>
2024-06-16 04:32:25 -0700

    First commit

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks=[
+    {
+        labels:'Home',
+        imgurl:'/icons/Home.svg',
+        route:'/'
+    },
+    {
+        labels:'Upcoming',
+        imgurl:'/icons/upcoming.svg',
+        route:'/upcoming'
+    },
+    {
+        labels:'Previous',
+        imgurl:'/icons/previous.svg',
+        route:'/previous'
+    },
+    {
+        labels:'Recordings',
+        imgurl:'/icons/Video.svg',
+        route:'/recordings'
+    },
+    {
+        labels:'Personal Room',
+        imgurl:'/icons/add-personal.svg',
+        route:'/personal-room'
+    },
+]
